575fab98ea5306165471ec1425b9881676aab864
Ryan Vaughn <34794279+Ryan-Vaughn@users.noreply.github.com>
2023-12-19 11:13:40 -0500

    Slogging through refactoring

--- Design_Patterns_Update/managers.py
+++ Design_Patterns_Update/managers.py
@@ -1,11 +1,14 @@
 import numpy as np
 from abc import ABC, abstractmethod
 
-class Manager(ABC):
+class Distributor(ABC):
     """
-    Abstract class for Manager.
+    Abstract class for Distributor
 
-    Managers distribute computations to cells. They compute local and global
+    Distributors break up whole datasets of particles and distribute them into
+    a specified type of cell. When a cell method is called through the Manager,
+    the Distributor collects the output of the cell method and stores it in a 
+    manner consistent with the corresponding particles position in the whole dataset.
     """
     @abstractmethod
     def __init__(self):
@@ -35,118 +38,125 @@ class Manager(ABC):
         """
         pass
 
-class ManagerSPH2D(Manager):
-    def __init(self):
+class DistributorSPH2D(Distributor):
+    """
+    Distributor for distributing computations into Cells.
+    """        
+    def __init__(self,x,v,bounds,eps,CellType):        
+        self.num_pts,self.dim = x.shape
+        self.eps = eps
 
-        self.cell_bounds = None
-        self.cells_loc_dict = None
-        self.cells_hash_dict = None
-        self.cells_hash_dict_inv = None
-
-        self.neighbors_hash_id = None
+        self.x = x
+        self.v = v
+        self.x_id = None
+        self.x_g = None
+        self.cuts = None
 
         self.x_neighbors = None
         self.v_neighbors = None
-        self.neighbors_hash_id = None
-        self.nonempty_cell_hashes = None
-        self.nonempty_cell_neighbors_hashes = None
+        self.neighbors_id = None
+        self.neighbors_g = None
+        self.neighbors_cuts = None
 
-    def initialize_cells(self):
+        self.nonempty_cells_id = None
+        self.nonempty_cell_neighbors_id = None
+
+        self.id_cells_dict = self.initialize_lookup(CellType, bounds, eps)
+
+        self.sort_indices = None
+        self.sort_neighbors_indices = None
+
+    def initialize_lookup(self, CellType, bounds, eps):
         """
-        Procedure to create a dictionary  of all cells in the simulation 
-        linked to their integer grid coordinates. Also constructs lookup
-        dictionaries from hash values to integer coordinates.
+        Procedure to create dictionaries which index the Cells in a grid.
+
+        In the 2d SPH case, this is done by constructing a cell for each
+        pair of integers inside of the given rectangular domain, then 
+        applying a bijective map between the set of integers and the 
+        set of pairs of integers, resulting in a unique integer id for 
+        each cell.
         """
+        grid_bounds = np.ceil(bounds/eps)
+        grid_x_coords = range(-1,int(grid_bounds[0,0]) + 1)
+        grid_y_coords = range(-1,int(grid_bounds[0,1]) + 1)
 
-        self.cell_bounds = np.ceil(self.bounds/self.eps)
-        self.cell_x_coords = range(-1,int(self.cell_bounds[0,0]) + 1)
-        self.cell_y_coords = range(-1,int(self.cell_bounds[0,1]) + 1)
+        id_cells_dict = {int((grid_x_coords + grid_y_coords) * (grid_x_coords + grid_y_coords + 1) / 2 + grid_y_coords) : CellType()}
 
-        #dictionary which maps integer multiples of epsilon to the coresponding Cell
-        self.cells_loc_dict = { (x,y) : Cell((x,y)) 
-                           for x in self.cell_x_coords 
-                           for y in self.cell_y_coords}
+        return id_cells_dict
 
-        self.cells_hash_dict = { (x,y) :  int((x + y) * (x + y + 1) / 2 + y)
-                           for x in self.cell_x_coords 
-                           for y in self.cell_y_coords}
-        
-        self.cells_hash_dict_inv = { h : (x,y) for (x,y),h 
-                                    in self.cells_hash_dict.items() }
-        
-        # Dictionary mapping hash values to corresponding Cell
-        self.cells_dict = { k : self.cells_loc_dict[v] for k,v in self.cells_hash_dict_inv.items()}
-
-        
-    def assign_particles(self):
-        self.sort_particles()
-        self.get_nonempty_cells()
+    def assign_particles(self,x,eps):
+        """
+        """
+        self.sort_particles(x,eps)
         self.map_particles()
         self.sort_neighbor_particles()
         self.get_nonempty_neighbors()
         self.map_neighbors()
-
-    def sort_particles(self):
+    
+    def sort_particles(self,x,eps):
+        """
+        Procedure which generates x_id, which gives the corresponding id to each particle in x.
+        """
         # map X to grid
-        self.G = np.rint(self.X / self.eps).astype(int)
-        # map grid coordinates to hash ID.
-        self.x_g = self.G[:,0]
-        self.y_g = self.G[:,1]
+        self.x_g = np.rint(x / eps).astype(int)
+        self.x_id = self.map_to_id(self.x_g)
 
-        # Applying hash to G
-        self.hash_id = ((self.x_g + self.y_g) * (self.x_g + self.y_g + 1) / 2 + self.y_g).astype(int)
-        # arg sort hash IDs
-        self.sort_indices = np.argsort(self.hash_id)
-        # sort hash_id
-        self.hash_id = self.hash_id[self.sort_indices]
+
+        self.sort_indices = np.argsort(self.x_id)
+
+        self.x_id = self.x_id[self.sort_indices]
+        self.x = self.x[self.sort_indices,:]
+        self.x_g = self.x_g[self.sort_indices,:]
+        self.v = self.v[self.sort_indices,:]
+
+        self.nonempty_cells_id = np.unique(self.x_id)
         
-        self.X = self.X[self.sort_indices,:]
-        # We need this sort for the later neighbors computation
-        self.G = self.G[self.sort_indices,:]
-        self.V = self.V[self.sort_indices,:]
-
-    def get_nonempty_cells(self):
-        self.nonempty_cell_hashes = np.unique(self.hash_id)
+    def map_to_id(self,x):
+        """
+        Helper function mapping integer pairs to a unique integer point.
+        """
+        a = x[:,0]
+        b = x[:,1] 
+        return ((a + b) * (a + b + 1) / 2 + b).astype(int)
 
     def map_particles(self):
-        self.cuts = np.searchsorted(self.hash_id, self.nonempty_cell_hashes)
+        """
+        
+        """
+        self.cuts = np.searchsorted(self.x_id, self.nonempty_cells_id)
         # needs to add the endpoint
-        self.cuts = np.append(self.cuts,self.num_pts)
+        self.cuts = np.append(self.cuts,self.dim)
         
         # for each unique hash ID:
         
-        for i in range(self.nonempty_cell_hashes.shape[0]):
-            hash = self.nonempty_cell_hashes[i]
+        for i in range(self.nonempty_cells_id.shape[0]):
+            id = self.nonempty_cells_id[i]
 
-            X_cell = self.X[self.cuts[i]:self.cuts[i + 1],:]
-            V_cell = self.V[self.cuts[i]:self.cuts[i + 1],:]
+            X_cell = self.x[self.cuts[i]:self.cuts[i + 1],:]
+            V_cell = self.v[self.cuts[i]:self.cuts[i + 1],:]
              
             # Assign X_cell to the cell corresponding to hash
-            self.cells_dict[hash].populate(X_cell,V_cell)
+            self.id_cells_dict[id].populate(X_cell,V_cell)
 
     def sort_neighbor_particles(self):
         num_neighbors = 9
+        _eps_e1 = np.arange(2)
+        _eps_e2 = np.arange(1,-1,-1)
+        _ul = -1 * _eps_e1 + _eps_e2
+        _um = _eps_e2
+        _ur = _eps_e1 + _eps_e2
+        _ml = -1 * _eps_e1
+        _mm = np.zeros(2)
+        _mr = _eps_e1
+        _bl = -1 * _eps_e1 + -1 * _eps_e2
+        _bm = -1 * _eps_e2
+        _br = _eps_e1 + -1 * _eps_e2
 
-        # generate a list of direction vectors for the grid.
-        eps_e1 = np.arange(2)
-        eps_e2 = np.arange(1,-1,-1)
-
-        ul = -1 * eps_e1 + eps_e2
-        um = eps_e2
-        ur = eps_e1 + eps_e2
-        ml = -1 * eps_e1
-        mm = np.zeros(2)
-        mr = eps_e1
-        bl = -1 * eps_e1 + -1 * eps_e2
-        bm = -1 * eps_e2
-        br = eps_e1 + -1* eps_e2
-
-        direction_vectors = np.array([ul,um,ur,ml,mm,mr,bl,bm,br])
-
+        direction_vectors =  np.array([_ul,_um,_ur,_ml,_mm,_mr,_bl,_bm,_br])
         # Copy X num_neighbors times (num_neighbors depends on dim)
         # This copy is to book keep positions.
         x_copies = np.repeat(self.x[:, :, np.newaxis], num_neighbors, axis=2)
-        g_copies = np.repeat(self.g[:, :, np.newaxis], num_neighbors, axis=2)
+        g_copies = np.repeat(self.x_g[:, :, np.newaxis], num_neighbors, axis=2)
         v_copies = np.repeat(self.v[:, :, np.newaxis], num_neighbors, axis=2)
         
         # collapse neighboring grid points to grid points offset by each neighboring direction.
@@ -159,12 +169,10 @@ class ManagerSPH2D(Manager):
         self.v_neighbors = v_copies.transpose(2,0,1).reshape(-1,self.dim)
         
         # Apply hash to full_cells
-        x_g = cells_copies[:,0]
-        y_g = cells_copies[:,1]
-        self.neighbors_hash_id = ((x_g + y_g) * (x_g + y_g + 1) / 2 + y_g).astype(int)
+        self.neighbors_id = self.map_to_id(cells_copies)
         
         # argsort hash values
-        self.neighbors_sort_indices = np.argsort(self.neighbors_hash_id)
+        self.neighbors_sort_indices = np.argsort(self.neighbors_id)
         
         # sort X and V copies with the arg
         self.x_neighbors = self.x_neighbors[self.neighbors_sort_indices,:]
@@ -192,4 +200,5 @@ class ManagerSPH2D(Manager):
              
             # Assign X_cell to the cell corresponding to hash
             self.cells_dict[hash].set_neighbors(x_cell_neighbors,
-                                                v_cell_neighbors)
+                                                v_cell_neighbors)\
+
